c53363c515fe7b99a3cca9ed354fdb26ab7b4202
Sang <Sang101174@gmail.com>
2024-08-14 14:22:15 +0700

    Update DBUtils.java

--- Assigment_PRJ301/src/java/utils/DBUtils.java
+++ Assigment_PRJ301/src/java/utils/DBUtils.java
@@ -14,9 +14,9 @@ import java.sql.SQLException;
  * @author Sang1011
  */
 public class DBUtils {
-    private static final String DB_NAME = "PRJ301_Assigment";
-    private static final String PASSWORD = "12345";
-    private static final String USER_NAME = "sa";
+    private static final String DB_NAME = "Oluna_Clothing";
+    private static final String PASSWORD = "your MySQL's Password";
+    private static final String USER_NAME = "your MySQL's name account";
 
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
         Connection conn = null;
